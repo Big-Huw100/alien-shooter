0fe4b1fe522f0094d8b4ae4297a594edf329245d
Huw Williams <huwmorgan91@gmail.com>
2023-04-25 12:28:09 +0100

    Revised display and shoot functionality.

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -29,21 +29,70 @@ function () {
 
 ; // Elements
 
-var alienContainer = document.querySelector(".alien-container"); // Aliens
+var atkAlienContainers = document.querySelectorAll(".atk-alien-container");
+var defAlienContainers = document.querySelectorAll(".def-alien-container");
+var motherAlienContainers = document.querySelectorAll(".mother-alien-container"); // Aliens
 
 var atkShip = new Alien("Zekvel", "./images/attack-ship.png", 45);
 var defShip = new Alien("Ungan", "./images/defense-ship.png", 80);
-var motherShip = new Alien("Zoneth", "./images/mother-ship.png", 100); // Display Aliens
+var motherShip = new Alien("Zoneth", "./images/mother-ship.png", 100);
 
-alienContainer.innerHTML = atkShip.getAlienCard(); // Gameplay
-// click on an alien ship, it looses health
+var _loop = function _loop(i) {
+  // Display Aliens
+  var atkShip = new Alien("Zekvel", "./images/attack-ship.png", 45);
+  atkAlienContainers[i].innerHTML = atkShip.getAlienCard(); // Gameplay
 
-alienContainer.addEventListener("click", function () {
-  atkShip.hp = atkShip.hp - 12;
+  atkAlienContainers[i].addEventListener("click", function () {
+    atkShip.hp = atkShip.hp - 12;
 
-  if (atkShip.hp > 0) {
-    alienContainer.innerHTML = atkShip.getAlienCard();
-  } else {
-    alienContainer.innerHTML = "";
-  }
-});
+    if (atkShip.hp > 0) {
+      atkAlienContainers[i].innerHTML = atkShip.getAlienCard();
+    } else {
+      atkAlienContainers[i].innerHTML = "";
+    }
+  });
+};
+
+for (var i = 0; i < atkAlienContainers.length; i++) {
+  _loop(i);
+}
+
+var _loop2 = function _loop2(_i) {
+  // Display Aliens
+  var defShip = new Alien("Ungan", "./images/defense-ship.png", 80);
+  defAlienContainers[_i].innerHTML = defShip.getAlienCard(); // Gameplay
+
+  defAlienContainers[_i].addEventListener("click", function () {
+    defShip.hp = defShip.hp - 10;
+
+    if (defShip.hp > 0) {
+      defAlienContainers[_i].innerHTML = defShip.getAlienCard();
+    } else {
+      defAlienContainers[_i].innerHTML = "";
+    }
+  });
+};
+
+for (var _i = 0; _i < defAlienContainers.length; _i++) {
+  _loop2(_i);
+}
+
+var _loop3 = function _loop3(_i2) {
+  // Display Aliens
+  var motherShip = new Alien("Zoneth", "./images/mother-ship.png", 100);
+  motherAlienContainers[_i2].innerHTML = motherShip.getAlienCard(); // Gameplay
+
+  motherAlienContainers[_i2].addEventListener("click", function () {
+    motherShip.hp = motherShip.hp - 9;
+
+    if (motherShip.hp > 0) {
+      motherAlienContainers[_i2].innerHTML = motherShip.getAlienCard();
+    } else {
+      motherAlienContainers[_i2].innerHTML = "";
+    }
+  });
+};
+
+for (var _i2 = 0; _i2 < motherAlienContainers.length; _i2++) {
+  _loop3(_i2);
+}
--- script.js
+++ script.js
@@ -21,7 +21,9 @@ class Alien {
 
 // Elements
 
-const alienContainer = document.querySelector(".alien-container");
+const atkAlienContainers = document.querySelectorAll(".atk-alien-container");
+const defAlienContainers = document.querySelectorAll(".def-alien-container");
+const motherAlienContainers = document.querySelectorAll(".mother-alien-container");
 
 // Aliens
 
@@ -29,19 +31,48 @@ const atkShip = new Alien("Zekvel", "./images/attack-ship.png", 45);
 const defShip = new Alien("Ungan", "./images/defense-ship.png", 80);
 const motherShip = new Alien("Zoneth", "./images/mother-ship.png", 100);
 
-// Display Aliens
+for (let i = 0; i < atkAlienContainers.length; i++) {
+    // Display Aliens
+    const atkShip = new Alien("Zekvel", "./images/attack-ship.png", 45);
+    atkAlienContainers[i].innerHTML = atkShip.getAlienCard();
+    // Gameplay
+    atkAlienContainers[i].addEventListener("click", () => {
+        atkShip.hp = atkShip.hp - 12;
+        if (atkShip.hp > 0) {
+            atkAlienContainers[i].innerHTML = atkShip.getAlienCard();
+        } else {
+            atkAlienContainers[i].innerHTML = "";
+        }
+    });
+}
 
-alienContainer.innerHTML = atkShip.getAlienCard();
+for (let i = 0; i < defAlienContainers.length; i++) {
+    // Display Aliens
+    const defShip = new Alien("Ungan", "./images/defense-ship.png", 80);
+    defAlienContainers[i].innerHTML = defShip.getAlienCard();
+    // Gameplay
+    defAlienContainers[i].addEventListener("click", () => {
+        defShip.hp = defShip.hp - 10;
+        if (defShip.hp > 0) {
+            defAlienContainers[i].innerHTML = defShip.getAlienCard();
+        } else {
+            defAlienContainers[i].innerHTML = "";
+        }
+    });
+}
 
-// Gameplay
+for (let i = 0; i < motherAlienContainers.length; i++) {
+    // Display Aliens
+    const motherShip = new Alien("Zoneth", "./images/mother-ship.png", 100);
+    motherAlienContainers[i].innerHTML = motherShip.getAlienCard();
+    // Gameplay
+    motherAlienContainers[i].addEventListener("click", () => {
+        motherShip.hp = motherShip.hp - 9;
+        if (motherShip.hp > 0) {
+            motherAlienContainers[i].innerHTML = motherShip.getAlienCard();
+        } else {
+            motherAlienContainers[i].innerHTML = "";
+        }
+    });
+}
 
-// click on an alien ship, it looses health
-
-alienContainer.addEventListener("click", () => {
-    atkShip.hp = atkShip.hp - 12;
-    if (atkShip.hp > 0) {
-        alienContainer.innerHTML = atkShip.getAlienCard();
-    } else {
-        alienContainer.innerHTML = "";
-    }
-});
